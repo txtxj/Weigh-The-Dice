bc1769e4bc7bf193728bf677ec130e1ed0db5968
txtxj <1418373194@qq.com>
2022-07-16 10:24:15 +0800

    feat: complete moving and camera following

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public GameObject player;
+
+	public float speed = 0.1f;
+	public int playerStress = 4;
+
+	private Vector3 dir = new Vector3(-5f, 12f, -5f);
+	
+	public void ResetPosition(GameObject cplayer)
+	{
+		if (cplayer != null)
+		{
+			player = cplayer;
+			transform.position = player.transform.position + dir;
+		}
+		else
+		{
+			transform.position = dir;
+		}
+	}
+
+	private void Start()
+	{
+		ResetPosition(null);
+	}
+
+	private void Update()
+	{
+		if (Camera.main != null && player != null)
+		{
+			if (Input.GetMouseButtonDown(1))
+			{
+				dir.x *= -1;
+			}
+			Vector3 target = (player.transform.position * playerStress) / (playerStress + 1) + dir;
+			transform.position = Vector3.Lerp(transform.position, target, speed);
+			transform.LookAt(player.transform);
+		}
+	}
+}
--- Assets/Scripts/DiceMove.cs
+++ Assets/Scripts/DiceMove.cs
@@ -2,26 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DiceMove : MonoBehaviour
 {
     public Vector2Int pos;
-    public int headNumber;
     public float moveTime;
 
     private Vector3 rotate;
-    private Vector3 lastRotate;
+    private Quaternion lastRotate;
     private static int animFlag = 0;
+    private bool finishRotate = true;
+
+    public int[] faceNumber = new int[] { 2, 5, 1, 6, 3, 4 };
 
     public void SignalFlag()
     {
         animFlag += 1;
+        finishRotate = true;
     }
 
     private void MoveAnimation(Vector3 position)
     {
-        animFlag -= 1;
+        animFlag -= 2;
         Hashtable hash = new Hashtable();
         hash.Add("position", position);
         hash.Add("time", moveTime);
@@ -30,33 +35,47 @@ public class DiceMove : MonoBehaviour
         iTween.MoveTo(gameObject, hash);
     }
 
+    private void ChainSwap(int a, int b, int c, int d)
+    {
+        int k = faceNumber[a];
+        faceNumber[a] = faceNumber[d];
+        faceNumber[d] = faceNumber[c];
+        faceNumber[c] = faceNumber[b];
+        faceNumber[b] = k;
+    }
+
     private void Walk(int dir)
     {
         if (MapInfo.Movable(pos.x, pos.y, dir))
         {
-            Vector3 rot = Vector3.zero;
+            Vector3 rot = -MapInfo.GetRotation(pos.x, pos.y);
             switch (dir)
             {
                 case 0:
                     pos += Vector2Int.left;
-                    rot = new Vector3(0f, 0f, 90f);
+                    rot += new Vector3(0f, 0f, 90f);
+                    ChainSwap(0, 4, 1, 5);
                     break;
                 case 1:
                     pos += Vector2Int.right;
-                    rot = new Vector3(0f, 0f, -90f);
+                    rot += new Vector3(0f, 0f, -90f);
+                    ChainSwap(0, 5, 1, 4);
                     break;
                 case 2:
                     pos += Vector2Int.down;
-                    rot = new Vector3(-90f, 0f, 0f);
+                    rot += new Vector3(-90f, 0f, 0f);
+                    ChainSwap(0, 3, 1, 2);
                     break;
                 case 3:
                     pos += Vector2Int.up;
-                    rot = new Vector3(90f, 0f, 0f);
+                    rot += new Vector3(90f, 0f, 0f);
+                    ChainSwap(0, 2, 1, 3);
                     break;
             }
             MoveAnimation(MapInfo.GetPosition(pos.x, pos.y));
-            lastRotate = transform.eulerAngles;
             rotate = MapInfo.GetRotation(pos.x, pos.y) + rot;
+            finishRotate = false;
+            Debug.Log(faceNumber[0]);
         }
     }
 
@@ -64,13 +83,21 @@ public class DiceMove : MonoBehaviour
     {
         transform.position = MapInfo.GetPosition(pos.x, pos.y);
         rotate = Vector3.zero;
+        lastRotate = transform.rotation;
     }
 
     private void FixedUpdate()
     {
-        if (animFlag < 0f)
+        if (finishRotate == false)
         {
             transform.Rotate(rotate * Time.deltaTime / moveTime, Space.World);
+        }
+        else if (animFlag < 0)
+        {
+            finishRotate = true;
+            transform.rotation = lastRotate * Quaternion.Inverse(lastRotate) * Quaternion.Euler(rotate) * lastRotate;
+            lastRotate = transform.rotation;
+            animFlag += 1;
         }
     }
 
